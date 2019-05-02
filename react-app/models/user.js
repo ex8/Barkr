4ebe686ea645ac9c0b4a03463c2dc3b80650f37e
GaryStraub <gstraub@sfsu.edu>
2019-05-01 20:24:16 -0700

    genesis

---
+++ react-app/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    First_Name: {
+        type: String,
+        required: true
+    },
+    Last_Name: {
+        type: String,
+        required: true
+    }
+
+
+});
+const User = mongoose.model
