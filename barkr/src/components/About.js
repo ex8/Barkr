0819b8779279c389ea93356b9867869f7b10a6fb
sharan144 <sharanlao@gmail.com>
2019-05-08 12:17:56 -0700

    About completed, PetNavigator(home) not done

--- barkr/src/components/About.js
+++ barkr/src/components/About.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import '../style.css';
 
 const About = () => (
     <div>
-        about barkr
+        <img src="https://images.pexels.com/photos/4933/lake-animals-dogs-pets.jpg" alt="" height="300px" width="500px"/>
+        <h1>About Barkr</h1>
+        <p>
+            Barkr will allow you to find your pet a playdate.
+        </p>
+        <p>
+            If you are trying to meet new people and other animal lovers that has a pet or just trying to let your pet socialize, you've come to the right place.
+        </p>
+        <p>
+            Just swipe right on any fabulous pets you'd like your pet to go on a date with.
+        </p>
+        <p>
+        <a href="./Signup">Sign up now to get started!</a>
+        </p> 
     </div>
 );
 
--- barkr/src/components/PetNavigator.js
+++ barkr/src/components/PetNavigator.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import '../style.css';
+
 
 const PetNavigator = () => (
-    <div>
-        show pets around
-    </div>
+
+<div class="card">
+  <div class="container">
+  <p>image here</p>
+    <h1>Name</h1> 
+    <h2>Breed</h2>
+    <p>Text here</p> 
+    <button class="btn btn-red">&#x2716;</button>
+<button class="btn btn-green">&#x2714;</button>
+  </div>
+</div>
+
+
 );
 
 export default PetNavigator;
