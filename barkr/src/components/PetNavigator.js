359b1d49cc1e8fda0573ab772fbdecc957aa8a62
sharan144 <sharanlao@gmail.com>
2019-05-08 18:10:37 -0700

    updated Homepage CSS

--- barkr/src/components/PetNavigator.js
+++ barkr/src/components/PetNavigator.js
@@ -5,17 +5,17 @@ import '../style.css';
 const PetNavigator = () => (
 
 <div class="card">
+  <img src="https://images.pexels.com/photos/4933/lake-animals-dogs-pets.jpg" alt="Photo of pet"></img>
   <div class="container">
-  <p>image here</p>
     <h1>Name</h1> 
-    <h2>Breed</h2>
+    <h2><b>Breed</b></h2> 
     <p>Text here</p> 
     <button class="btn btn-red">&#x2716;</button>
-<button class="btn btn-green">&#x2714;</button>
+    {/* need to implement on click action  */}
+    <div class="divider"/>
+    <button class="btn btn-green">&#x2714;</button>
   </div>
 </div>
-
-
 );
 
 export default PetNavigator;
