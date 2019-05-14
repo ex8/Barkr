550b03e2077028bdcc5741e65e22d08bd0bfab10
GaryStraub <gstraub@sfsu.edu>
2019-05-14 14:16:37 -0700

    Upload works...
    Commit before refactor of image upload on profile.js

--- barkr/src/components/Profile.js
+++ barkr/src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -6,14 +6,14 @@ import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import {Person, AccountCircle, Label, Edit} from '@material-ui/icons';
+import { Person, AccountCircle, Label, Edit } from '@material-ui/icons';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
-import {CircularProgress, TextField} from "@material-ui/core";
-import {petLogin} from '../redux/actions/petActions';
-import {connect} from 'react-redux';
-import {withRouter} from 'react-router-dom';
+import { CircularProgress, TextField } from "@material-ui/core";
+import { petLogin } from '../redux/actions/petActions';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
@@ -36,7 +36,7 @@ const styles = theme => ({
     },
     root: {
         display: 'flex',
-      },
+    },
     paper: {
         marginTop: theme.spacing.unit * 8,
         display: 'flex',
@@ -63,22 +63,24 @@ const styles = theme => ({
         marginRight: theme.spacing.unit,
     },
 });
-const states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
 class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            profilePic: null,
+            uploadImg: null,
             firstName: '',
             LastName: '',
             email: '',
             password: '',
             dobb: '',
-            address : '',
-            unit : '',
-            city : '',
-            state : '',
-            zip : '',
+            address: '',
+            unit: '',
+            city: '',
+            state: '',
+            zip: '',
             loading: false,
             editOn: false,
             errors: {}
@@ -93,13 +95,13 @@ class Profile extends Component {
 
     onFormSubmit = e => {
         e.preventDefault();
-        this.setState({loading: true});
+        this.setState({ loading: true });
         const pet = {
             email: this.state.email,
             password: this.state.password
         };
         this.props.petLogin(pet, this.props.history);
-        this.setState({loading: false});
+        this.setState({ loading: false });
     };
 
     componentWillReceiveProps = nextProps => {
@@ -115,99 +117,126 @@ class Profile extends Component {
             this.props.history.push(`/`);
         }
     };
+    fileSelectedHandler = event => {
+        this.setState({
+            profilePic: URL.createObjectURL(event.target.files[0]),
+        })
+        console.log(event.target.files[0]);
+    }
+    fileUPloadHandler = () =>{
+
+    }
 
     render() {
-        const {classes} = this.props;
-        return (            
+        const { classes } = this.props;
+        return (
             <main className={classes.root}>
                 <div className={classes.main}>
-                <CssBaseline />
-                <Paper className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <AccountCircle/>
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Profile
+                    <CssBaseline />
+                    <Paper className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <AccountCircle />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Profile
                     </Typography>
-                    <Button 
-                            onClick={() => { this.setState({editOn: true}); }}
+                        <Button
+                            onClick={() => { this.setState({ editOn: true }); }}
                             type="submit"
                             variant="contained"
                             color="primary"
                             className={classes.submit}>
-                            {this.state.loading && <CircularProgress size={25} color={'primary'}/>}
+                            {this.state.loading && <CircularProgress size={25} color={'primary'} />}
                             <Edit className={classes.leftIcon} />
                             {!this.state.editOn && "Edit Profile"}
                             {this.state.editOn && "Cancel"}
-                    </Button>
+                        </Button>
 
-                    <form className={classes.container} onSubmit={this.onFormSubmit}>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="firstName">First Name</InputLabel>
-                            <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
-                                   name="firstName" autoComplete="lastName" autoFocus/>
-                        </FormControl>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="email">Last Name</InputLabel>
-                            <Input id="LastName" value={this.state.lastName} onChange={this.onInputChange}
-                                   name="firstName" autoComplete="lastName" autoFocus/>
-                        </FormControl>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="email">Email Address</InputLabel>
-                            <Input id="email" value={this.state.email} onChange={this.onInputChange}
-                                   name="email" autoComplete="email" autoFocus/>
-                        </FormControl>
-                        <TextField
-                            id="date"
-                            label="Birthday"
-                            type="date"
-                            defaultValue="2017-05-24"
-                            margin="normal"
-                            fullWidth
-                            className={classes.textField}
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="Address">Address</InputLabel>
-                            <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
-                                name="firstName" autoComplete="lastName" autoFocus />
-                        </FormControl>
-                        <FormControl margin="normal" fullWidth>
-                            <InputLabel htmlFor="Address">Unit</InputLabel>
-                            <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
-                                name="firstName" autoComplete="lastName" autoFocus />
-                        </FormControl>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="Address">City</InputLabel>
-                            <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
-                                name="firstName" autoComplete="lastName" autoFocus />
-                        </FormControl>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="Address">State</InputLabel>
-                            <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
-                                name="firstName" autoComplete="state" autoFocus />
-                        </FormControl>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="Address">Zip</InputLabel>
-                            <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
-                                name="firstName" autoComplete="lastName" autoFocus />
-                        </FormControl>
-                        {this.state.editOn &&
-                            <Button
-                                type="submit"
+                        <form className={classes.container} onSubmit={this.onFormSubmit}>
+                            {
+                                this.state.profilePic !== null && <img width="100%" src={this.state.profilePic} />
+                            }
+                            
+                            <input
+                                accept="image/*"
+                                className={classes.input}
+                                style={{ display: 'none' }}
+                                id="raised-button-file"
+                                multiple
+                                type="file"
+                                onChange={this.fileSelectedHandler}
+                            />
+                            <label htmlFor="raised-button-file">
+                                <Button variant="raised" component="span" className={classes.button}>
+                                    Upload
+                                    </Button>
+                            </label>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="firstName">First Name</InputLabel>
+                                <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
+                                    name="firstName" autoComplete="lastName" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Last Name</InputLabel>
+                                <Input id="LastName" value={this.state.lastName} onChange={this.onInputChange}
+                                    name="firstName" autoComplete="lastName" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input id="email" value={this.state.email} onChange={this.onInputChange}
+                                    name="email" autoComplete="email" autoFocus />
+                            </FormControl>
+                            <TextField
+                                id="date"
+                                label="Birthday"
+                                type="date"
+                                defaultValue="2017-05-24"
+                                margin="normal"
                                 fullWidth
-                                variant="contained"
-                                color="primary"
-                                className={classes.submit}>
-                                {!this.state.loading && 'Submit'}
-                                {this.state.loading && <CircularProgress size={25} color={'primary'} />}
-                            </Button>
-                        }
+                                className={classes.textField}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="Address">Address</InputLabel>
+                                <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
+                                    name="firstName" autoComplete="lastName" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" fullWidth>
+                                <InputLabel htmlFor="Address">Unit</InputLabel>
+                                <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
+                                    name="firstName" autoComplete="lastName" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="Address">City</InputLabel>
+                                <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
+                                    name="firstName" autoComplete="lastName" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="Address">State</InputLabel>
+                                <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
+                                    name="firstName" autoComplete="state" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="Address">Zip</InputLabel>
+                                <Input id="FirsName" value={this.state.firstName} onChange={this.onInputChange}
+                                    name="firstName" autoComplete="lastName" autoFocus />
+                            </FormControl>
+                            {this.state.editOn &&
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.submit}>
+                                    {!this.state.loading && 'Submit'}
+                                    {this.state.loading && <CircularProgress size={25} color={'primary'} />}
+                                </Button>
+                            }
 
-                    </form>
-                </Paper>
+                        </form>
+                    </Paper>
                 </div>
             </main>
         );
@@ -219,8 +248,8 @@ const mapStateToProps = state => ({
     auth: state.auth
 });
 
-const mapDispatchToProps = {petLogin};
+const mapDispatchToProps = { petLogin };
 
 export default withStyles(styles)(connect(
     mapStateToProps, mapDispatchToProps
-    )(withRouter(Profile)));
+)(withRouter(Profile)));
