3cb2b1d74bf716be41731742068de2e70d7f3096
Matt M <matt@puroxy.org>
2019-05-17 17:55:33 -0700

    added electron to load react app

--- barkr/src/redux/store.js
+++ barkr/src/redux/store.js
@@ -2,12 +2,18 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/index';
 import thunk from 'redux-thunk';
 
+// for REDUX DEV TOOLS
+// const store = createStore(
+//     rootReducer,
+//     compose(
+//         applyMiddleware(thunk), 
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//         )
+// );
+
 const store = createStore(
     rootReducer,
-    compose(
-        applyMiddleware(thunk), 
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-        )
+    applyMiddleware(thunk)
 );
 
 export default store;
